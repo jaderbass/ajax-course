ee21439dbdd5a7822b2a321a0a775d04741b9ba7
Jörg Aderhold <joerg@jaderbass.de>
2026-07-15 10:46:32 +0200

    feat: add comments to app.js

--- kap2/03-ajax-util/js/app.js
+++ kap2/03-ajax-util/js/app.js
@@ -10,22 +10,35 @@ document.querySelector("#btnLoad").addEventListener("click", _ => {
     const description = document.querySelector(".description");
     description.classList.add("day");
     
+    // Überschrifts-Element anlegen und Inhaltstext hinzufügen
     const heading = document.createElement("h2");
     heading.textContent = data.course;
+
+    // Absatzelement anlegen und Klassen-Attribut hinzufügen
     const courseDay = document.createElement("p");
     const courseAttr = document.createAttribute("class");
     courseAttr.value = "hint";
+
+    // Attributknoten erzeugen
     courseDay.setAttributeNode(courseAttr);
     courseDay.textContent = `Tag ${data.day}`;
+
     const topic = document.createElement("h3");
     const topicAttr = document.createAttribute("class");
     topicAttr.value = "subtitle";
     topic.textContent = data.topic;
+
+    // Listen-Element anlegen
     const ul = document.createElement("ul");
+    // Items-Array speichern
     const items = data.items;
+    // Schleife über das Items-Array
     items.forEach(item => {
+      // List-Item-Element anlegen
       const li = document.createElement("li");
+      // Text hinzugefügt
       li.textContent = item.title;
+      // List-Item-Element als Kind-Element an die Liste angefügt
       ul.appendChild(li);
     });
 
