d9fba68a6498a0e31a05cc9b9d68ac65c20dd3ad
Jörg Aderhold <joerg@jaderbass.de>
2026-07-15 11:10:53 +0200

    fix: kap2/04 in broken.js and copy server/data.json from kap2

--- kap2/04-debug-tasks/js/broken.js
+++ kap2/04-debug-tasks/js/broken.js
@@ -1,7 +1,7 @@
 // TODO: Warum funktioniert das nicht?
 
 const xhr = new XMLHttpRequest();
-xhr.open("GET", "server/data.jso"); // Tippfehler!
+xhr.open("GET", "server/data.json"); // Tippfehler!
 xhr.onload = () => {
   const data = JSON.parse(xhr.responseText);
   console.log(data);
